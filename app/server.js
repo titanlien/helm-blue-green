65e47cae0751170041e837633a1d294813b7d17d
MH Lien <titan@mhlien.tw>
2020-08-03 12:08:30 +0200

    add app/ directory

---
+++ app/server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+var podname = process.env.HOSTNAME;
+var version = process.env.VERSION;
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Running on: ' + podname + " | ver:" + version);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
